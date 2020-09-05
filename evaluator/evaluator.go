d72e8d05dbf837281b196597a16cfde43079a57f
Tuhin Nair <tuhinnair444@gmail.com>
2020-09-05 20:06:17 +0700

    add function object and first test for representation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -50,6 +50,10 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Body: body, Env: env}
 	case *ast.ReturnStatement:
 		val := Eval(node.ReturnValue, env)
 		if isError(val) {
